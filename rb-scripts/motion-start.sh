c585f91ac751e6e8f7094c0be60a291b498279c3
Rosario B <rossa_4057@hotmail.com>
2015-06-08 22:44:13 +0200

    Improved Script Logic

--- rb-scripts/motion-start.sh
+++ rb-scripts/motion-start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nohup ~/mmal/motion-mmal -n -c motion-mmalcam.conf 1>/dev/null 2>&1 </dev/null &
+pid=$(pidof motion-mmal)
+if [ -z "$pid" ]; then	
+	nohup ~/mmal/motion-mmal -n -c motion-mmalcam.conf 1>/dev/null 2>&1 </dev/null &
+fi
--- rb-scripts/stream-start.sh
+++ rb-scripts/stream-start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-raspivid -o - -t 0 -w 800 -h 600 -fps 30 |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264
+pid=$(pidof raspivid)
+if [ -z "$pid" ]; then
+	raspivid -o - -t 0 -w 800 -h 600 -fps 30 |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264
+fi
